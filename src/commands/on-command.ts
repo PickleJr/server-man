7969f6899dff95e0552f219522711779aeb997ec
PickleJr <picklejr.t.gonzalez@gmail.com>
2021-03-02 21:04:06 -0600

    Prints server information on server start

--- src/commands/on-command.ts
+++ src/commands/on-command.ts
@@ -1,6 +1,8 @@
+import publicIp from 'public-ip';
 import { Message, TextChannel } from 'discord.js';
 import { ICommand } from '../models/command.model';
 import { ServerService } from '../services/server-service';
+import { scripts } from '../config.json';
 
 export class OnCommand implements ICommand {
   readonly name: string = 'on';
@@ -19,6 +21,7 @@ export class OnCommand implements ICommand {
         isServerFound = true;
         v.toggleService(true, message.channel as TextChannel, message.member.voice.channel);
         message.channel.send(`Server status for ${k}: ${v.isServiceRunning ? 'Up' : 'Down'}`);
+        this.printServerInfo(message.channel as TextChannel);
         message.channel.send('Please wait a few minutes for the server to become discoverable.');
       } else if (args.includes(k.toLowerCase())) {
         isServerFound = true;
@@ -29,5 +32,10 @@ export class OnCommand implements ICommand {
     if (!isServerFound) {
       message.channel.send(`What the HECK are you talking about. I don't know what server you're asking for`);
     }
+  }
+
+  private printServerInfo = async (channel: TextChannel) => {
+    channel.send(`No easy URL yet!\nIP to go to is\`${(await publicIp.v4())}:${scripts.valheim.port}\`.`);
+    channel.send(`Password: \`${scripts.valheim.password}\``);
   }
 }
